09f4204bec5e72d8aeaa2f8bd3aa88011c0c8fdf
AbelSyntax <abel-r@live.se>
2025-02-22 11:10:54 +0100

    first commit

---
+++ P1/node.js
@@ -0,0 +1,39 @@
+// länkar api och nycklen till js filen 
+const url =
+	'https://api.openweathermap.org/data/2.5/weather';
+const apiKey =
+	'f00c38e0279b7bc85480c3fe775d518c';
+
+$(document).ready(function (){
+weatherFn('Pune')});
+//för att hämta data från väder api
+async function weatherFn(cName) {
+    const temp = `${url}?q=${cName}&appid=${apiKey}&units=metric`;
+    try {
+        const res = await fetch(temp);
+        const data = await res.json();
+        if (res.ok) {
+            weatherFn(data);
+        } else {
+            alert('stad hittades inte. försök igen');
+        }
+    } catch (error) {
+        console.error('Fel vid hämtning av väderdata:', error);
+    }     
+}
+// länkar väder daten med tags från css och lägger till tid's format med Moment.js
+function weatherShowFn(data) {
+	$('#city-name').text(data.name);
+	$('#date').text(moment().
+		format('MMMM Do YYYY, h:mm:ss a'));
+	$('#temperature').
+		html(`${data.main.temp}°C`);
+	$('#description').
+		text(data.weather[0].description);
+	$('#wind-speed').
+		html(`Wind Speed: ${data.wind.speed} m/s`);
+	$('#weather-icon').
+		attr('src',
+			`...`);
+	$('#weather-info').fadeIn();
+}
